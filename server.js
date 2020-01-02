0520cd86c703fd2d8b8bf9d44526ba49433dd310
pranathi99 <pranathi.ganti@gmail.com>
2020-01-02 11:44:47 +0530

    first checkin

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const path=require('path')
+const path = require('path')
 const port = process.env.PORT||3000
 
-app.use(express.static(__dirname+'/dist/project')
-app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/project/index.html')))
+app.use(express.static(__dirname+'/dist/project');
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/project/index.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
